b6036d3c6378fd8f31dbc330a4599dad391d0bf3
Phreework <phreework@gmail.com>
2018-04-01 20:57:42 +0800

    Add PathTool
    pathtool.cs is a editor script
    its used to create path in IDE

---
+++ Unity td test/Assets/Editor/PathTool.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PathTool : ScriptableObject {
+    static PathNode m_parent = null;
+    [MenuItem("PathTool/Create PathNode")]
+    static void CreatePathNode() {
+        //create a new pathnode
+        GameObject go = new GameObject();
+        go.AddComponent<PathNode>();
+        go.name = "pathnode";
+        //set tag
+        go.tag = "pathnode";
+        //select pathnode
+        Selection.activeTransform = go.transform;
+    }
+    [MenuItem("PathTool/Set Parent %q")]
+    static void SetParent() {
+        if (!Selection.activeGameObject || Selection.GetTransforms(SelectionMode.Unfiltered).Length > 1)
+            return;
+        if (Selection.activeGameObject.tag.CompareTo("pathnode") == 0) {
+            m_parent = Selection.activeGameObject.GetComponent<PathNode>();
+        }
+    }
+    [MenuItem("PathTool/Set Next %w")]
+    static void SetNextChild() {
+        if (!Selection.activeGameObject || m_parent == null || Selection.GetTransforms(SelectionMode.Unfiltered).Length > 1) return;
+        if (Selection.activeGameObject.tag.CompareTo("pathnode") == 0) {
+            m_parent.setNext(Selection.activeGameObject.GetComponent<PathNode>());
+            m_parent = null;
+        }
+    }
+
+
+}
