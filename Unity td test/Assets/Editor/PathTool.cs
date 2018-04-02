67cb5042e428210a669fdfb5054ff363e7c5bc8e
Phreework <phreework@gmail.com>
2018-04-02 14:00:13 +0800

    make PathTool autowork
    make PathTool autowork in Create Method

--- Unity td test/Assets/Editor/PathTool.cs
+++ Unity td test/Assets/Editor/PathTool.cs
@@ -5,21 +5,36 @@ using UnityEditor;
 
 public class PathTool : ScriptableObject {
     static PathNode m_parent = null;
+    static int PathNodeNumber = 0;
     [MenuItem("PathTool/Create PathNode")]
     static void CreatePathNode() {
         //create a new pathnode
         GameObject go = new GameObject();
         go.AddComponent<PathNode>();
         go.name = "pathnode";
-        //set tag
+        go.name = "pathnode" + PathNodeNumber;
         go.tag = "pathnode";
         //select pathnode
         Selection.activeTransform = go.transform;
+        SetSelfNextNode();
+        SetSelfParentNode();
+        PathNodeNumber++;
     }
+
+    private static void SetSelfParentNode() {
+        //set self parent
+        m_parent = Selection.activeGameObject.GetComponent<PathNode>();
+    }
+
+    private static void SetSelfNextNode() {
+        if (PathNodeNumber != 0) {
+            m_parent.setNext(Selection.activeGameObject.GetComponent<PathNode>());
+        }
+    }
+
     [MenuItem("PathTool/Set Parent %q")]
     static void SetParent() {
-        if (!Selection.activeGameObject || Selection.GetTransforms(SelectionMode.Unfiltered).Length > 1)
-            return;
+        if (!Selection.activeGameObject || Selection.GetTransforms(SelectionMode.Unfiltered).Length > 1) return;
         if (Selection.activeGameObject.tag.CompareTo("pathnode") == 0) {
             m_parent = Selection.activeGameObject.GetComponent<PathNode>();
         }
@@ -32,6 +47,10 @@ public class PathTool : ScriptableObject {
             m_parent = null;
         }
     }
+    [MenuItem("PathTool/name zero %e")]
+    static void SetPathnodenumberZero() {
+        PathNodeNumber = 0;
+        }
 
 
 }
